67004eac7bc45ee5e9c04cddb9803ca10a57953b
Joseph Manning <jgmanning3@yahoo.com>
2021-09-25 16:38:17 -0700

    Add dates to history page

--- src/components/pages/History.js
+++ src/components/pages/History.js
@@ -1,13 +1,23 @@
 import React from "react";
 
 function History(props) {
-    return (
-      <div className="history">
+  return (
+    <div className="history">
+      <div>
+        <h2 className="m-3">History</h2>
+      </div>
+      <div class="timeline">
         <div>
-          <h2 className="m-3">History</h2>
+          <h2>August 16, 2014</h2>
+          <p>First watch party as the Arizona Spurs</p>
+        </div>
+        <div>
+          <h2>August 13, 2016</h2>
+          <p>First watch party at our current home, Fibber Magees</p>
         </div>
       </div>
-    );
-  }
-  
-  export default History;
+    </div>
+  );
+}
+
+export default History;
