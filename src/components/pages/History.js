b09abcb8f8f4a827d6667a6c598fd70aff788dde
Joseph Manning <jgmanning3@yahoo.com>
2021-10-13 18:12:04 -0700

    Create timeline dot animation move right

--- src/components/pages/History.js
+++ src/components/pages/History.js
@@ -2,8 +2,8 @@ import React from "react";
 import styles from "../../styleSheets/styleHistory.module.css";
 
 var id = null;
-function myMove() {
-  var elem = document.getElementById("timeDot");   
+function myMoveLeft() {
+  var elem = document.getElementById("timeDotLeft");   
   var pos = 0;
   clearInterval(id);
   id = setInterval(frame, 10);
@@ -18,6 +18,23 @@ function myMove() {
   }
 }
 
+function myMoveRight() {
+  var elem = document.getElementById("timeDotRight");   
+  var pos = 0;
+  clearInterval(id);
+  id = setInterval(frame, 10);
+  function frame() {
+    if (pos === 200) {
+      clearInterval(id);
+    } else {
+      pos++; 
+      // elem.style.top = pos + 'px'; 
+      elem.style.left = pos + 'px'; 
+    }
+  }
+}
+
+
 function History(props) {
   return (
     <div className="history">
@@ -26,19 +43,22 @@ function History(props) {
       </div>
       <div className={styles.timeline}>
         <div className={styles.left} >
-          <div className={styles.container} id="timeDot" onClick={myMove}></div>
+          <div className={styles.container} id="timeDotLeft" onClick={myMoveLeft}></div>
           <div className={styles.content}>
             <h2>August 16, 2014</h2>
             <p>First watch party as the Arizona Spurs</p>
           </div>
         </div>
-        <div className={`${styles.container} ${styles.left}`}>
+        <div className={styles.timeline}>
+        <div className={styles.right}>
+          <div className={styles.container} id="timeDotRight" onClick={myMoveRight}></div>
           <div className={styles.content}>
             <h2>August 13, 2016</h2>
             <p>First watch party at our current home, Fibber Magees</p>
           </div>
         </div>
       </div>
+    </div>
     </div>
   );
 }
