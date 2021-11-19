e4e32321ffc765fe8620fa4a009db8099aba3be3
Joseph Manning <jgmanning3@yahoo.com>
2021-11-19 15:12:34 -0700

    Create init member page

---
+++ src/components/pages/Member.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Member(props) {
+  return (
+    <div className="shop">
+      <div>
+        <h2 className="m-3">Member Dashboard</h2>
+      </div>
+      
+    </div>
+  );
+}
+
+export default Member;
