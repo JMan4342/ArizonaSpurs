fb53b2c20c09381c75c6ae316174f899ff0d61f0
Joseph Manning <jgmanning3@yahoo.com>
2021-11-19 15:25:32 -0700

    Add content to Members page

--- src/components/pages/Member.js
+++ src/components/pages/Member.js
@@ -6,7 +6,24 @@ function Member(props) {
       <div>
         <h2 className="m-3">Member Dashboard</h2>
       </div>
-      
+      <div>
+        <p>
+          The Arizona Spurs member page will feature member only benefits that
+          include discounts to the Official Spurs shop, access to tickets, early
+          access to Arizona Spurs merchandise, and club news.
+        </p>
+      </div>
+      <div>
+        <h3>
+          Spurs Shop Discount Codes
+        </h3>
+        <h4>
+          **************
+        </h4>
+        <p>
+          Enter this code in the discount code section at checkout.
+        </p>
+      </div>
     </div>
   );
 }
