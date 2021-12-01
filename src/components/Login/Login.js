210b357585b4fc7668e5912522a8b7f1395a4efd
Joseph Manning <jgmanning3@yahoo.com>
2021-12-01 13:55:40 -0700

    Create server page for login, route to login page

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,13 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
+import PropTypes from "prop-types";
 
-export default function Login() {
+async function loginUser(credentials) {
+  return fetch("http://localhost:8080/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(credentials),
+  }).then((data) => data.json());
+}
+
+export default function Login({ setToken }) {
+  const [password, setPassword] = useState();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const token = await loginUser({
+      password,
+    });
+    setToken(token);
+  };
+
   return (
     <div>
       <h1>Please Enter Password</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label>
           <p>Password</p>
-          <input type="password" />
+          <input
+            type="password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
         </label>
         <div>
           <button type="submit">Submit</button>
@@ -16,3 +40,7 @@ export default function Login() {
     </div>
   );
 }
+
+Login.propTypes = {
+  setToken: PropTypes.func.isRequired,
+};
