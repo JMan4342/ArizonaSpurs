f5199999bf189d0424682704c0cf13a54e5d2b40
Joseph Manning <jgmanning3@yahoo.com>
2021-12-02 19:35:58 -0700

    Integrate sign-in API to Login page

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,4 +1,6 @@
 import React, { useState } from 'react';
+import axios from 'axios';
+import { setUserSession } from '../../Utils/Common';
 
 function Login(props) {
     const username = useFormInput('');
@@ -6,8 +8,19 @@ function Login(props) {
     const [error, setError] = useState(null);
     const [loading, setLoading] = useState(false);
 
+    // handle button click of login form
     const handleLogin = () => {
-        props.history.push('/member');
+        setError(null);
+        setLoading(true);
+        axios.post('http://localhost:4000/users/signin', { username: username.value, password: password.value }).then(response => {
+            setLoading(false);
+            setUserSession(response.data.token, response.data.user);
+            props.history.push('/member');
+        }).catch(error => {
+            setLoading(false);
+            if (error.response.status === 401) setError(error.response.data.message);
+            else setError("Something went wrong. Please try again later.");
+        })
     }
 
     return (
